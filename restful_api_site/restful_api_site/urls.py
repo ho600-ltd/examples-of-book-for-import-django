824cc7a24acc0d62ee56bd59095f38876a7dfd46
hoamon <hoamon@ho600.com>
2020-01-18 01:30:10 +0800

    set up router in restful_api_site/urls.py @book-01-part-master]

--- restful_api_site/restful_api_site/urls.py
+++ restful_api_site/restful_api_site/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from data_store.views import FlowDataModelViewSet
+router = routers.DefaultRouter()
+router.register('flowdata', FlowDataModelViewSet, basename="flowdata")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/',
+         include((router.urls, 'data_store_api_root'),
+                  namespace="data_store_api_root")),
 ]
