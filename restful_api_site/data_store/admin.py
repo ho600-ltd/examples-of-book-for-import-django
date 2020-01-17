a9fa501644c075d016a312d50d835f811f3ee586
hoamon <hoamon@ho600.com>
2020-01-17 21:22:12 +0800

    register two models with ModelAdmin class @book-01-part-master]

--- restful_api_site/data_store/admin.py
+++ restful_api_site/data_store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from data_store.models import EndSpot, FlowData
+
+class EndSpotAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(EndSpot, EndSpotAdmin)
+
+class FlowDataAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(FlowData, FlowDataAdmin)
