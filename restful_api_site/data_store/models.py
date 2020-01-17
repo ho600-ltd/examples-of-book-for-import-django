c6e82a5bcda2d38501f235226e151dd08b2acd0f
hoamon <hoamon@ho600.com>
2020-01-17 20:54:46 +0800

    Add two models: EndSpot, FlowData @book-01-part-master]

--- restful_api_site/data_store/models.py
+++ restful_api_site/data_store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class EndSpot(models.Model):
+    topic = models.CharField(max_length=150, unique=True)
+    note = models.TextField()
+
+    class Meta:
+        permissions = (
+            ('add_flowdata_under_this_end_spot', 'Add FlowData records under This EndSpot'),
+        )
+
+class FlowData(models.Model):
+    end_spot = models.ForeignKey(EndSpot, on_delete=models.CASCADE)
+    timestamp = models.DecimalField(max_digits=20, decimal_places=6, db_index=True)
+    value = models.FloatField() #IFNO: in some cases, DecimalField is better
+    create_time = models.DateTimeField(auto_now_add=True, db_index=True)
