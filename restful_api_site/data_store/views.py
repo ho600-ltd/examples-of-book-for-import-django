520ae9e52eaccc3c2f24fcd149ab03459c0579ac
hoamon <hoamon@ho600.com>
2020-01-18 01:29:09 +0800

    Add FlowDataModelViewSet @book-01-part-master]

--- restful_api_site/data_store/views.py
+++ restful_api_site/data_store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
+from data_store.models import FlowData
+from data_store.serializers import FlowDataSerializer
 
-# Create your views here.
+
+
+class FlowDataModelViewSet(viewsets.ModelViewSet):
+    queryset = FlowData.objects.all()
+    serializer_class = FlowDataSerializer
+    renderer_classes = (BrowsableAPIRenderer, JSONRenderer, )
+    http_method_names = ('get', 'post', )
